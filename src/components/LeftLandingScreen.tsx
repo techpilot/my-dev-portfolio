bc0d6b894d659ec1c133d680a2f5ef76cb56e47d
techpilot <ngwustephen99@gmail.com>
2025-12-05 14:03:47 +0100

    added case study link

--- src/components/LeftLandingScreen.tsx
+++ src/components/LeftLandingScreen.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import circular_search from "@/assets/svgs/circular_search.svg";
 import sec_dotted from "@/assets/svgs/sec_dotted.svg";
 import zigzag_sec from "@/assets/svgs/zigzag_sec.svg";
+import Link from "next/link";
 
 export default function LeftLandingScreen() {
     return (
@@ -24,18 +25,29 @@ export default function LeftLandingScreen() {
             </div>
 
             <div
-                className="px-12 md:px-0 md:ml-14 lg:ml-28 xl:ml-36 mt-32 lg:mt-52 flex flex-col justify-center md:justify-between w-full md:w-[30rem] lg:w-[35rem]">
+                className="px-12 md:px-0 md:ml-14 lg:ml-28 xl:ml-36 mt-32 md:mt-20 lg:mt-32 xl:mt-40 flex flex-col justify-center md:justify-between w-full md:w-[30rem] lg:w-[35rem]">
                 <div>
                     <h1 className="text-5xl md:text-3xl lg:text-6xl font-bold lg:leading-16 text-[var(--secondary)]">Software <br/> Engineer
                         <span
                             className="bg-white rounded-full h-2.5 w-2.5 inline-block -ml-1"></span>
                     </h1>
-                    <p className="text-white text-base mt-5 md:w-[23rem] lg:w-[30rem]">I focus on writing
+                    <p className="text-white text-base mt-3 md:w-[23rem] lg:w-[30rem] cursor-pointer">I focus on writing
                         clean,
                         efficient
                         code
                         and thrive
-                        in collaborative environments.</p>
+                        in collaborative environments.
+                    </p>
+
+                    <div className="relative h-16">
+                        <Link
+                            href={'/spox'}
+                            className="z-[100] absolute inset-0 px-3 lg:px-5 py-3 border-2 rounded-full font-medium flex items-center transition-all hover:shadow-lg text-sm lg:text-base max-w-max mt-4"
+                            style={{borderColor: '#CCF381', color: '#CCF381'}}>
+                            Spox Case Study
+                            {/*<Github className="w-5 h-5 mr-2"/>*/}
+                        </Link>
+                    </div>
                 </div>
 
                 <div className="flex flex-row gap-6 md:gap-12 mb-10 md:mb-0 mt-20 md:mt-12 lg:mt-28 w-full">
